681043b0185db7c9a8387f063dd143cba108e0ac
Your Name <s026134@students.lmsd.org>
2023-10-12 17:24:22 -0400

    got rid of extraneous print statements and fixed functionality to see isAnonymous field in API

--- src/topics/create.js
+++ src/topics/create.js
@@ -33,7 +33,7 @@ module.exports = function (Topics) {
             lastposttime: 0,
             postcount: 0,
             viewcount: 0,
-            isAnonymous: false, //this is the default value of isAnonymous
+            isAnonymous: data.isAnonymous == null ? false : data.isAnonymous,
         };
 
         if (Array.isArray(data.tags) && data.tags.length) {
--- test/topics.js
+++ test/topics.js
@@ -82,38 +82,6 @@ describe('Topic\'s', () => {
             });
         });
 
-        it('should create anonymous topic/post', (done) => {
-            topics.post({
-                uid: topic.userId,
-                title: topic.title,
-                content: topic.content,
-                cid: topic.categoryId,
-                isAnonymous: true,
-            }, (err, result) => {
-                assert.ifError(err);
-                assert(result);
-                topic.tid = result.topicData.tid;
-                assert(result.topicData.isAnononymous);
-                done();
-            });
-        });
-
-        it('should create a not anonymous topic/post', (done) => {
-            topics.post({
-                uid: topic.userId,
-                title: topic.title,
-                content: topic.content,
-                cid: topic.categoryId,
-                isAnononymous: false,
-            }, (err, result) => {
-                assert.ifError(err);
-                assert(result);
-                topic.tid = result.topicData.tid;
-                assert.equal(result.topicData.isAnononymous, false);
-                done();
-            });
-        });
-
         it('should get post count', (done) => {
             socketTopics.postcount({ uid: adminUid }, topic.tid, (err, count) => {
                 assert.ifError(err);
