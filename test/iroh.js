40fc9211f5acc7543189b64e3fbe11b42144e586
seking03 <seking@andrew.cmu.edu>
2023-10-27 17:44:26 -0400

    Fixed as many linter errors as possible

--- test/iroh.js
+++ test/iroh.js
@@ -4,6 +4,7 @@ const Iroh = require('iroh');
 
 
 // test taken from test/files/2.js
+// @ts-ignore
 const stage = new Iroh.Stage(`
 function foo(name, age) {
   return 'The person known as "' + name + '" is ' + age + ' years old';
@@ -16,47 +17,51 @@ foo('Sophia', 20);
 // function call
 stage.addListener(Iroh.CALL)
     .on('before', (e) => {
-      const external = e.external ? '#external' : '';
-      console.log(' '.repeat(e.indent) + 'call', e.name, external, '(', e.arguments, ')');
-      //console.log(e.getSource());
+        const external = e.external ? '#external' : '';
+        // @ts-ignore
+        console.log(' '.repeat(e.indent) + 'call', e.name, external, '(', e.arguments, ')');
+        // console.log(e.getSource());
     })
     .on('after', (e) => {
-      const external = e.external ? '#external' : '';
-      console.log(' '.repeat(e.indent) + 'call', e.name, 'end', external, '->', [e.return]);
-      //console.log(e.getSource());
+        const external = e.external ? '#external' : '';
+        // @ts-ignore
+        console.log(' '.repeat(e.indent) + 'call', e.name, 'end', external, '->', [e.return]);
+        // console.log(e.getSource());
     });
 
 // function
 stage.addListener(Iroh.FUNCTION)
-  .on('enter', (e) => {
-    const sloppy = e.sloppy ? '#sloppy' : '';
-    if (e.sloppy) {
-      console.log(' '.repeat(e.indent) + 'call', e.name, sloppy, '(', e.arguments, ')');
-      //console.log(e.getSource());
-    }
-  })
-  .on('leave', (e) => {
-    const sloppy = e.sloppy ? '#sloppy' : '';
-    if (e.sloppy) {
-      console.log(' '.repeat(e.indent) + 'call', e.name, 'end', sloppy, '->', [void 0]);
-      //console.log(e.getSource());
-    }
-  })
-  .on('return', (e) => {
-    const sloppy = e.sloppy ? '#sloppy' : '';
-    if (e.sloppy) {
-      console.log(' '.repeat(e.indent) + 'call', e.name, 'end', sloppy, '->', [e.return]);
-      //console.log(e.getSource());
-    }
-  });
+    .on('enter', (e) => {
+        const sloppy = e.sloppy ? '#sloppy' : '';
+        if (e.sloppy) {
+            console.log(' '.repeat(e.indent) + 'call', e.name, sloppy, '(', e.arguments, ')');
+            // console.log(e.getSource());
+        }
+    })
+    .on('leave', (e) => {
+        const sloppy = e.sloppy ? '#sloppy' : '';
+        if (e.sloppy) {
+            // @ts-expect-error
+            console.log(' '.repeat(e.indent) + 'call', e.name, 'end', sloppy, '->', [void 0]);
+            // console.log(e.getSource());
+        }
+    })
+    .on('return', (e) => {
+        const sloppy = e.sloppy ? '#sloppy' : '';
+        if (e.sloppy) {
+            console.log(' '.repeat(e.indent) + 'call', e.name, 'end', sloppy, '->', [e.return]);
+            // console.log(e.getSource());
+        }
+    });
 
 // program
 stage.addListener(Iroh.PROGRAM)
-  .on('enter', (e) => {
-    console.log(' '.repeat(e.indent) + 'Program');
-  })
-  .on('leave', (e) => {
-    console.log(' '.repeat(e.indent) + 'Program end', '->', e.return);
-  });
+    .on('enter', (e) => {
+        console.log(' '.repeat(e.indent) + 'Program');
+    })
+    .on('leave', (e) => {
+        console.log(' '.repeat(e.indent) + 'Program end', '->', e.return);
+    });
 
+// @ts-ignore
 eval(stage.script);
